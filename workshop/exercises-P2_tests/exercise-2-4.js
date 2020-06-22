ee6380a34f3965a517118c71db84bdaa5d69a825
Marie Foxall <marie@wastedeffort.ca>
2020-06-22 12:43:14 -0400

    Exercise 2-4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,19 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  const splitString = str.split("");
+  return splitString[index];
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("bye", 4), undefined);
+expect(getLetterAtIndex("48,973,621", 6), ",");
+expect(getLetterAtIndex("-7632", 4), "2");
+expect(getLetterAtIndex("how many tests can we do", 12), "t");
+expect(getLetterAtIndex("turns out a whole bunch WOW", -1), undefined);
+expect(getLetterAtIndex("last-one", 4), "-");
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +31,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
