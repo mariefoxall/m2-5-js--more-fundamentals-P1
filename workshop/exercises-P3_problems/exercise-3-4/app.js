191522a9b536c624ffeb13bedafd2cf165eba81a
Marie Foxall <marie@wastedeffort.ca>
2020-06-22 21:05:16 -0400

    Exercise 3-4

--- workshop/exercises-P3_problems/exercise-3-4/app.js
+++ workshop/exercises-P3_problems/exercise-3-4/app.js
@@ -1,2 +1,20 @@
 // Exercise 3-4 - The GRID
-// ------------
+const cellsPerSide = 8;
+const board = document.querySelector("#board");
+board.style.width = "600px";
+board.style.height = "600px";
+board.style.gridTemplateRows = `repeat(${cellsPerSide}, 1fr)`;
+board.style.gridTemplateColumns = `repeat(${cellsPerSide}, 1fr)`;
+
+// for (i = 1; i <= cellsPerSide; i++) {
+//   const row = document.createElement("div");
+//   row.setAttribute("id", `row-${i}`);
+//   //   row.setAttribute("class", "form");
+//   board.appendChild(row);
+for (j = 1; j <= cellsPerSide * cellsPerSide; j++) {
+  const cell = document.createElement("div");
+  cell.setAttribute("id", `cell-${j}`);
+  cell.setAttribute("class", "cell");
+  board.appendChild(cell);
+  console.log(cell.id);
+}
